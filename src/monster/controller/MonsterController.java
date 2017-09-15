90d8f6082469b3acd73bfded44499a8474390770
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-15 13:05:36 -0600

    creating code that gets rid of my monsters arms legs and eyes.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -7,9 +7,15 @@ public class MonsterController
 	
 	public void start()
 	{
-		// MarshmallowMonster basic = new MarshmallowMonster();
-		// System.out.println(basic);
-		MarshmallowMonster YeahBoi = new MarshmallowMonster("Weird YeahBoi Monster", 10, 8, 8.5, true);
+		MarshmallowMonster basic = new MarshmallowMonster();
+		System.out.println(basic);
+		MarshmallowMonster YeahBoi = new MarshmallowMonster("Weird YeahBoi Monster", 15, 4
+				, 4.5, true);
 		System.out.println(YeahBoi);
+		System.out.println("I am feeling hungry, I am going to eat one of fred's arms");
+		YeahBoi.setArmCount(YeahBoi.getArmCount() - 3);
+		System.out.println(YeahBoi);
+		System.out.println("Hey I need more... Give Me MORE!");
+		YeahBoi.setTentacleAmount(YeahBoi.getTentacleAmount() - 1);
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -25,11 +25,62 @@ public class MarshmallowMonster
 		this.hasBloop = hasBloop;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
+	
 	public String toString()
 	{
 		String description = "Eyy Boi, my name is " + name + ", I contain " + eyeCount + " eye holes inside of the skullular region, I also contain ";
 		description += tentacleAmount + " tentacles but I have " + armCount + " swole arms!. " + hasBloop + " communicates to you ";
-		description += " if I have some sort of bloop";
+		description += "if I have some sort of bloop";
 		
 		return description;
 	}
